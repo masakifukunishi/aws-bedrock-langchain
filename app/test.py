1bb66005ceadb07d1a0cc758aaaed5041a87868f
MasakiFukunishi <fmsknn@gmail.com>
2024-06-23 01:18:12 +0000

    add hello

--- app/test.py
+++ app/test.py
@@ -1,3 +1,7 @@
-print("test.py")
-
 from langchain_community.llms import Bedrock
+import boto3
+
+client = boto3.client("bedrock-runtime")
+model = Bedrock(client=client, model_id="anthropic.claude-instant-v1")
+
+print(model("Hello, Who are you?"))
